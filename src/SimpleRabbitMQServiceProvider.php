d37f74be55547e4869e3e01610b64ff9d53efbb9
usmonaliyev99 <t.usmonaliyev99@gmail.com>
2024-11-04 14:04:10 +0500

    feat: SimpleRabbitMQServiceProvider is added

---
+++ src/SimpleRabbitMQServiceProvider.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Usmonaliyev\SimpleRabbit;
+
+use Illuminate\Support\ServiceProvider;
+use Usmonaliyev\SimpleRabbit\MQ\ConnectionManager;
+
+class SimpleRabbitMQServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap the application services.
+     */
+    public function boot(): void
+    {
+        if ($this->app->runningInConsole()) {
+            $this->publishes([__DIR__.'/../config/simple-mq.php' => config_path('simple-mq.php')], 'simple-mq');
+
+            $this->publishes([__DIR__.'/../routes/actions.php' => base_path('routes/actions.php')]);
+
+            // Registering package commands.
+            // $this->commands([]);
+        }
+    }
+
+    /**
+     * Register the application services.
+     */
+    public function register(): void
+    {
+        // Automatically apply the package configuration
+        $this->mergeConfigFrom(__DIR__.'/../config/simple-mq.php', 'simple-mq');
+
+        $this->app->singleton('simple-rabbitmq', fn () => new SimpleMQ);
+
+        $this->app->singleton(ActionMQ::class, fn () => new ActionMQ);
+        $this->app->singleton(ConnectionManager::class, fn () => new ConnectionManager);
+    }
+}
